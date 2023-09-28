977fb41c2824f7d4d8f8095b0aae674fbaa5681c
pradeeps0lanki <pradeepsolanki416@gmail.com>
2023-09-28 12:42:02 +0530

    page not found error fixed

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -18,7 +18,7 @@ export const PageNotFound = () => {
             <button className="text-5xl text-gray-700 font-bold my-10 dark:text-white flex justify-center ">Back To Home</button>
         </Link>
          
-          <img className="rounded" src="https://img.freepik.com/free-vector/oops-404-error-with-broken-robot-concept-illustration_114360-5529.jpg?size=626&ext=jpg&ga=GA1.2.1175487634.1692272762&semt=ais" alt="page not found" />
+          <h1>page not found</h1>
 
         </div>
         
